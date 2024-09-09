a5249710e78585b36fb3bf6c2445bab60df63b44
Muhammad Irfan Hassan <muhammad.irfan@xgrid.co>
2024-09-09 22:44:57 +0500

    test

--- S208/main.tf
+++ S208/main.tf
@@ -1,10 +1,8 @@
 # main.tf
-
 provider "aws" {
   region = "us-west-2"
 }
-
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  instance_type = "sa2.micro"
 }

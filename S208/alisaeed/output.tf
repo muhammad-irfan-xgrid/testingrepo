0706320c78826f3bd6a34d8a6516fa91ea6644fc
Muhammad Irfan Hassan <muhammad.irfan@xgrid.co>
2024-09-16 13:03:16 +0500

    fix code

--- S208/alisaeed/output.tf
+++
@@ -1,9 +0,0 @@
-output "database_endpoint" {
-  description = "RDS database endpoint"
-  value = module.db.db_instance_endpoint
-}
-
-output "DNS_endpoint" {
-  description = "Load balancer endpoint for the Wordpress Server"
-  value       = "http://${aws_lb.elb.dns_name}"
-}

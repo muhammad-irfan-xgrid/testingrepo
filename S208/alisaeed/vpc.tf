36eecaa11a8b73ab56203fdd08cf7b3170b38efe
Muhammad Irfan Hassan <muhammad.irfan@xgrid.co>
2024-09-16 14:36:26 +0500

    update file

--- S208/alisaeed/vpc.tf
+++ S208/alisaeed/vpc.tf
@@ -5,7 +5,7 @@ terraform {
 provider "aws" {
   region = "us-west-2"  # Adjust this to your preferred region
 }
-
+#adding commment to test
 # Create VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"  # Adjust CIDR block as needed
